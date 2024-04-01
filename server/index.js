cb8c5c2ddfaf0f83de2e0e41ef65dacf8bc4c8fe
dd2558 <dd2558@naver.com>
2024-04-01 16:12:26 +0900

    DB연동 성공

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const PORT = process.env.port || 8000;
+const cors = require("cors");
 
-app.get("/",(req,res)=>{
-  res.send("Hello world~");
+let corsOptions = {
+  origin: "*", // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+
+app.use(cors(corsOptions));
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "dd2558",
+  password: "gkehddnjs10",
+  database: "bbs2",
 });
 
-app.listen(PORT, ()=>{
-    console.log(`running on port ${PORT}`);
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT *FROM BOARD;";
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
 });
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
+
